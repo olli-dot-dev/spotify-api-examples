84c504ee03ea4efd55b27082070ce1be42796657
olli.dev <okluth@gmail.com>
2023-06-06 19:48:23 +0200

    retrieving Spotify track ID by using the Spotify API
    example of how to retrieve a Spotify track ID using basic python - Spotify API token required!

---
+++ get-spotify-track-id.py
@@ -0,0 +1,21 @@
+#retrieve Spotify API token first, then use it to get track ID
+#getting a track ID by searching for artist & track name
+
+import requests, json
+
+#Replace YourTrackName and Your%20artist with your track and artist name
+#if either has spaces, replace them with %20 as in the example
+#Searching for "YourTrackName" by "Your artist" in this example
+#replace DE with your country code
+query = "track:YourTrackName%20artist:Your%20artist&type=track&market=DE"
+songURL = f"https://api.spotify.com/v1/search?q={query}"
+headers = {
+    #token is the Spotify API token you retrieved earlier
+    "Authorization": "Bearer " + token
+}
+
+res = requests.get(url=songURL, headers=headers)
+
+tmp=res.json()
+track_id=tmp['tracks']['items'][0]['id']
+print(track_id)
